c4cb760f8a32b768506055979dadac1ab419f114
italo-de-souza <italo.souza@bandtec.com.br>
2021-06-14 18:41:50 -0300

    att

--- script/script1.sh
+++ script/script1.sh
@@ -61,7 +61,7 @@ abrir_software() {
 
     if [ \"$abrir\" == \"sim\" ]; 
     then
-        cd /home/$USER/Desktop/ac3-individual/jarProjeto
+        cd /home/$USER/Desktop/ac3-individual/jar/jarProjeto
 
         sleep 1
         echo "$(tput setaf 10)[Manivela]:$(tput setaf 7) Abrindo nossa aplicação"
